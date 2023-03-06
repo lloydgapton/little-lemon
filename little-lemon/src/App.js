5913f86ded44e7170733167de6e378b00a9a2df5
lloydgapton <lloydgapton123@gmail.com>
2023-03-06 23:19:16 +0100

    last commit

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header'
+import Main from './components/Main';
+import Footer from './components/Footer'
 function App() {
   return (
     <react.fragment className="App">Homepage
